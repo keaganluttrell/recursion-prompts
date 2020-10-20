16727e9fda6e10f5a15250dcecb25314fb02b451
Keagan Luttrell <keaganluttrell7@gmail.com>
2020-10-20 14:51:47 -0700

    multiplication complete

--- src/recursion.js
+++ src/recursion.js
@@ -153,7 +153,15 @@ var modulo = function (x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function (x, y) {
-
+  if (x === 0 || y === 0) {
+    return 0;
+  }
+  if ((x > 0 && y > 0) || (x < 0 && y > 0)) {
+    return x + multiply(x, y -= 1);
+  }
+  if ((x < 0 && y < 0) || (x > 0 && y < 0)) {
+    return -x + multiply(x, y += 1);
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
